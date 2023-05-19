c845b5d979e656249df08320db105eb28633e6f3
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-05-19 15:57:46 +0300

    feat: Adds make in progress rpc

--- proto/src/golang/describe_grpc.pb.go
+++ proto/src/golang/describe_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type DescribeServiceClient interface {
 	DeliverResult(ctx context.Context, in *DeliverResultRequest, opts ...grpc.CallOption) (*ResponseOK, error)
+	SetInProgress(ctx context.Context, in *SetInProgressRequest, opts ...grpc.CallOption) (*ResponseOK, error)
 	DeliverAWSResources(ctx context.Context, in *AWSResources, opts ...grpc.CallOption) (*ResponseOK, error)
 	DeliverAzureResources(ctx context.Context, in *AzureResources, opts ...grpc.CallOption) (*ResponseOK, error)
 }
@@ -38,6 +39,15 @@ func NewDescribeServiceClient(cc grpc.ClientConnInterface) DescribeServiceClient
 func (c *describeServiceClient) DeliverResult(ctx context.Context, in *DeliverResultRequest, opts ...grpc.CallOption) (*ResponseOK, error) {
 	out := new(ResponseOK)
 	err := c.cc.Invoke(ctx, "/kaytu.describe.v1.DescribeService/DeliverResult", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *describeServiceClient) SetInProgress(ctx context.Context, in *SetInProgressRequest, opts ...grpc.CallOption) (*ResponseOK, error) {
+	out := new(ResponseOK)
+	err := c.cc.Invoke(ctx, "/kaytu.describe.v1.DescribeService/SetInProgress", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -67,6 +77,7 @@ func (c *describeServiceClient) DeliverAzureResources(ctx context.Context, in *A
 // for forward compatibility
 type DescribeServiceServer interface {
 	DeliverResult(context.Context, *DeliverResultRequest) (*ResponseOK, error)
+	SetInProgress(context.Context, *SetInProgressRequest) (*ResponseOK, error)
 	DeliverAWSResources(context.Context, *AWSResources) (*ResponseOK, error)
 	DeliverAzureResources(context.Context, *AzureResources) (*ResponseOK, error)
 	mustEmbedUnimplementedDescribeServiceServer()
@@ -78,6 +89,9 @@ type UnimplementedDescribeServiceServer struct {
 
 func (UnimplementedDescribeServiceServer) DeliverResult(context.Context, *DeliverResultRequest) (*ResponseOK, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeliverResult not implemented")
+}
+func (UnimplementedDescribeServiceServer) SetInProgress(context.Context, *SetInProgressRequest) (*ResponseOK, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetInProgress not implemented")
 }
 func (UnimplementedDescribeServiceServer) DeliverAWSResources(context.Context, *AWSResources) (*ResponseOK, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeliverAWSResources not implemented")
@@ -112,6 +126,24 @@ func _DescribeService_DeliverResult_Handler(srv interface{}, ctx context.Context
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(DescribeServiceServer).DeliverResult(ctx, req.(*DeliverResultRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _DescribeService_SetInProgress_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SetInProgressRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DescribeServiceServer).SetInProgress(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/kaytu.describe.v1.DescribeService/SetInProgress",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DescribeServiceServer).SetInProgress(ctx, req.(*SetInProgressRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -162,6 +194,10 @@ var DescribeService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "DeliverResult",
 			Handler:    _DescribeService_DeliverResult_Handler,
+		},
+		{
+			MethodName: "SetInProgress",
+			Handler:    _DescribeService_SetInProgress_Handler,
 		},
 		{
 			MethodName: "DeliverAWSResources",
