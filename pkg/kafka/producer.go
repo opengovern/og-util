4b07361d5ccc9663f42ce9bf86fb2e134fed737c
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-07-20 16:54:44 +0400

    fix: Fixes kafka producer

--- pkg/kafka/producer.go
+++ pkg/kafka/producer.go
@@ -5,13 +5,25 @@ import (
 	"strings"
 )
 
-func NewKafkaProducer(kafkaServers []string) (*confluent_kafka.Producer, error) {
-	return confluent_kafka.NewProducer(&confluent_kafka.ConfigMap{
+func NewKafkaProducer(kafkaServers []string, configs map[string]interface{}) (*confluent_kafka.Producer, error) {
+	cm := confluent_kafka.ConfigMap{
 		"bootstrap.servers": strings.Join(kafkaServers, ","),
-		"acks":              "all",
-		"retries":           3,
-		"linger.ms":         1,
-		"batch.size":        1000000,
-		"compression.type":  "lz4",
+	}
+	if configs != nil {
+		for k, v := range configs {
+			cm[k] = v
+		}
+	}
+
+	return confluent_kafka.NewProducer(&cm)
+}
+
+func NewDefaultKafkaProducer(kafkaServers []string) (*confluent_kafka.Producer, error) {
+	return NewKafkaProducer(kafkaServers, map[string]interface{}{
+		"acks":             "all",
+		"retries":          3,
+		"linger.ms":        1,
+		"batch.size":       1000000,
+		"compression.type": "lz4",
 	})
 }
