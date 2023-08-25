a0ea50588d03bf375306f539bcdee4fcdd83132f
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-08-25 17:53:13 +0400

    feat: add GetCurrentFuncName

--- pkg/trace/jaeger.go
+++ pkg/trace/jaeger.go
@@ -1,7 +1,17 @@
 package trace
 
+import (
+	"fmt"
+	"runtime"
+)
+
 type TraceName string
 
 const (
 	DescribeWorkerTrace = "describe-worker"
 )
+
+func GetCurrentFuncName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return fmt.Sprintf("%s", runtime.FuncForPC(pc).Name())
+}
