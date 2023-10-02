698732db082045a4a8bce8d82456bb21a52aa125
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-10-02 17:28:24 +0400

    feat: add start steampipe service

--- pkg/steampipe/steampipe_config.go
+++ pkg/steampipe/steampipe_config.go
@@ -5,9 +5,12 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"go.uber.org/zap"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
+	"time"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -137,4 +140,63 @@ func GetStackElasticConfig(workspaceId string, stackId string) (config.ElasticSe
 		Username: "elastic",
 		Password: string(password),
 	}, nil
+}
+
+// StartSteampipeServiceAndGetConnection starts steampipe service and returns steampipe connection
+// NOTE: this function will only work on images that have steampipe installed
+func StartSteampipeServiceAndGetConnection(logger *zap.Logger) (*Database, error) {
+	for retry := 0; retry < 5; retry++ {
+		cmd := exec.Command("steampipe", "plugin", "list")
+		cmdOut, err := cmd.Output()
+		if err != nil {
+			logger.Error("plugin list failed", zap.Error(err), zap.String("body", string(cmdOut)))
+			time.Sleep(5 * time.Second)
+			if retry == 4 {
+				return nil, err
+			}
+			continue
+		}
+
+		break
+	}
+
+	cmd := exec.Command("steampipe", "service", "stop", "--force")
+	err := cmd.Start()
+	if err != nil {
+		logger.Error("first stop failed", zap.Error(err))
+		return nil, err
+	}
+	time.Sleep(5 * time.Second)
+	//NOTE: stop must be called twice. it's not a mistake
+	cmd = exec.Command("steampipe", "service", "stop", "--force")
+	err = cmd.Start()
+	if err != nil {
+		logger.Error("second stop failed", zap.Error(err))
+		return nil, err
+	}
+	time.Sleep(5 * time.Second)
+
+	cmd = exec.Command("steampipe", "service", "start", "--database-listen", "network", "--database-port",
+		"9193", "--database-password", "abcd")
+	cmdOut, err := cmd.Output()
+	if err != nil {
+		logger.Error("start failed", zap.Error(err), zap.String("body", string(cmdOut)))
+		return nil, err
+	}
+	time.Sleep(5 * time.Second)
+
+	logger.Info("steampipe service started")
+	steampipeConn, err := NewSteampipeDatabase(Option{
+		Host: "localhost",
+		Port: "9193",
+		User: "steampipe",
+		Pass: "abcd",
+		Db:   "steampipe",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Info("steampipe database created")
+	return steampipeConn, nil
 }
