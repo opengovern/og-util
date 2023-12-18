13aa4a5c9b45269615db380e513fe74417c06fda
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-12-18 14:16:14 +0400

    feat: Adds ingestion pipeline endpoint

--- pkg/describe/types.go
+++ pkg/describe/types.go
@@ -20,13 +20,15 @@ type DescribeJob struct {
 }
 
 type LambdaDescribeWorkerInput struct {
-	WorkspaceId      string      `json:"workspaceId"`
-	WorkspaceName    string      `json:"workspaceName"`
-	DescribeEndpoint string      `json:"describeEndpoint"`
-	KeyARN           string      `json:"keyARN"`
-	KeyRegion        string      `json:"keyRegion"`
-	KafkaTopic       string      `json:"kafkaTopic"`
-	DescribeJob      DescribeJob `json:"describeJob"`
+	WorkspaceId               string      `json:"workspaceId"`
+	WorkspaceName             string      `json:"workspaceName"`
+	DescribeEndpoint          string      `json:"describeEndpoint"`
+	IngestionPipelineEndpoint string      `json:"ingestionPipelineEndpoint"`
+	UseOpenSearch             bool        `json:"useOpenSearch"`
+	KeyARN                    string      `json:"keyARN"`
+	KeyRegion                 string      `json:"keyRegion"`
+	KafkaTopic                string      `json:"kafkaTopic"`
+	DescribeJob               DescribeJob `json:"describeJob"`
 }
 
 // Connector source.Type
