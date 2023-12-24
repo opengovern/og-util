e0dbfaa64908e2a4ab666ee0fc34bdf56e01c81b
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-12-24 18:05:26 +0400

    fix: Fixes es

--- pkg/kaytu-es-sdk/elasticsearch.go
+++ pkg/kaytu-es-sdk/elasticsearch.go
@@ -65,7 +65,7 @@ func CheckErrorWithContext(resp *opensearchapi.Response, ctx context.Context) er
 	if err != nil {
 		return fmt.Errorf("read error: %w", err)
 	}
-	
+
 	if ctx.Value(context_key.Logger) == nil {
 		fmt.Println("CheckErr data", string(data))
 	} else {
@@ -660,21 +660,26 @@ func (p *BaseESPaginator) CreatePit(ctx context.Context) (err error) {
 		p.client.PointInTime.Create.WithKeepAlive(1*time.Minute),
 		p.client.PointInTime.Create.WithContext(ctx),
 	)
-
-	if ctx.Value(context_key.Logger) == nil {
-		fmt.Println("PointInTime.Create pitRaw:", pitRaw, " pitRes:", pitRes, " err:", err)
-	} else {
-		plugin.Logger(ctx).Trace("PointInTime.Create pitRaw=%v pitRes=%v err=%v", pitRaw, pitRes, err)
-	}
+	//
+	//if ctx.Value(context_key.Logger) == nil {
+	//	fmt.Println("PointInTime.Create pitRaw:", pitRaw, " pitRes:", pitRes, " err:", err)
+	//} else {
+	//	plugin.Logger(ctx).Trace("PointInTime.Create pitRaw=%v pitRes=%v err=%v", pitRaw, pitRes, err)
+	//}
 
 	defer CloseSafe(pitRaw)
 	if err != nil && !strings.Contains(err.Error(), "illegal_argument_exception") {
+		if ctx.Value(context_key.Logger) == nil {
+			fmt.Println("PointInTime.Error err=", err, "pitRaw=", pitRaw)
+		} else {
+			plugin.Logger(ctx).Trace("PointInTime.CheckErr err=%v pitRaw=%v", err, pitRaw)
+		}
 		return err
 	} else if errIf := CheckErrorWithContext(pitRaw, ctx); errIf != nil || (err != nil && strings.Contains(err.Error(), "illegal_argument_exception")) {
 		if ctx.Value(context_key.Logger) == nil {
-			fmt.Println("PointInTime.CheckErr err=", err, "errIf=", errIf)
+			fmt.Println("PointInTime.CheckErr err=", err, "errIf=", errIf, "pitRaw=", pitRaw)
 		} else {
-			plugin.Logger(ctx).Trace("PointInTime.CheckErr err=", err, "errIf=", errIf)
+			plugin.Logger(ctx).Trace("PointInTime.CheckErr err=%v errIf=%v pitRaw=%v", err, errIf, pitRaw)
 		}
 
 		// try elasticsearch api instead
