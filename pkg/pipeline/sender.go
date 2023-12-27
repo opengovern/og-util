8ab9f92f4a57a8ed0c08ee332dbc47d8b5f7d50b
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-12-27 15:49:32 +0400

    fix: send to pipeline in batches

--- pkg/pipeline/sender.go
+++ pkg/pipeline/sender.go
@@ -20,53 +20,54 @@ func SendToPipeline(ingestionPipelineEndpoint string, resourcesToSend []kafka.Do
 		return nil
 	}
 
-	jsonResourcesToSend, err := json.Marshal(resourcesToSend)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest(
-		http.MethodPost,
-		ingestionPipelineEndpoint,
-		strings.NewReader(string(jsonResourcesToSend)),
-	)
-	req.Header.Add("Content-Type", "application/json")
-
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return err
-	}
-
-	creds, err := cfg.Credentials.Retrieve(context.Background())
-	if err != nil {
-		return err
-	}
-
-	signer := v4.NewSigner()
-	err = signer.SignHTTP(context.TODO(), creds, req,
-		fmt.Sprintf("%x", sha256.Sum256(jsonResourcesToSend)),
-		"osis", "us-east-2", time.Now())
-	if err != nil {
-		return err
-	}
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	defer resp.Body.Close()
-	// check status
-	if resp.StatusCode != http.StatusOK {
-		bodyStr := ""
-		if resp.Body != nil {
-			bodyBytes, err := io.ReadAll(resp.Body)
-			if err == nil {
-				bodyStr = string(bodyBytes)
-			}
+	for page := 0; page < len(resourcesToSend); page += 100 {
+		jsonResourcesToSend, err := json.Marshal(resourcesToSend[page:min(page+100, len(resourcesToSend))])
+		if err != nil {
+			return err
 		}
-		return fmt.Errorf("failed to send resources to OpenSearch, statusCode=%d, body=%s", resp.StatusCode, bodyStr)
-	}
 
+		req, err := http.NewRequest(
+			http.MethodPost,
+			ingestionPipelineEndpoint,
+			strings.NewReader(string(jsonResourcesToSend)),
+		)
+		req.Header.Add("Content-Type", "application/json")
+
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			return err
+		}
+
+		creds, err := cfg.Credentials.Retrieve(context.Background())
+		if err != nil {
+			return err
+		}
+
+		signer := v4.NewSigner()
+		err = signer.SignHTTP(context.TODO(), creds, req,
+			fmt.Sprintf("%x", sha256.Sum256(jsonResourcesToSend)),
+			"osis", "us-east-2", time.Now())
+		if err != nil {
+			return err
+		}
+
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+		// check status
+		if resp.StatusCode != http.StatusOK {
+			bodyStr := ""
+			if resp.Body != nil {
+				bodyBytes, err := io.ReadAll(resp.Body)
+				if err == nil {
+					bodyStr = string(bodyBytes)
+				}
+			}
+			return fmt.Errorf("failed to send resources to OpenSearch, statusCode=%d, body=%s", resp.StatusCode, bodyStr)
+		}
+	}
 	return nil
 }
