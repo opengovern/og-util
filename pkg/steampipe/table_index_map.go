661e4a67a09fde82a3fb7323ee7a42ce6b9d466e
artaasadi <arta-c@kaytu.io>
2024-03-19 15:22:25 +0100

    fix: add azuread conditional access policy

--- pkg/steampipe/table_index_map.go
+++ pkg/steampipe/table_index_map.go
@@ -5,12 +5,20 @@ import (
 )
 
 var AzureADKeys = map[string]struct{}{
-	strings.ToLower("Microsoft.Resources/users"):             {},
-	strings.ToLower("Microsoft.Resources/groups"):            {},
-	strings.ToLower("Microsoft.Resources/serviceprincipals"): {},
-	strings.ToLower("Microsoft.Resources/applications"):      {},
-	strings.ToLower("Microsoft.Resources/devices"):           {},
-	strings.ToLower("Microsoft.Resources/signInReports"):     {},
+	strings.ToLower("Microsoft.Resources/users"):                   {},
+	strings.ToLower("Microsoft.Resources/groups"):                  {},
+	strings.ToLower("Microsoft.Resources/serviceprincipals"):       {},
+	strings.ToLower("Microsoft.Resources/applications"):            {},
+	strings.ToLower("Microsoft.Resources/devices"):                 {},
+	strings.ToLower("Microsoft.Resources/signInReports"):           {},
+	strings.ToLower("Microsoft.Resources/domains"):                 {},
+	strings.ToLower("Microsoft.Resources/identityproviders"):       {},
+	strings.ToLower("Microsoft.Resources/securitydefaultspolicy"):  {},
+	strings.ToLower("Microsoft.Resources/authorizationpolicy"):     {},
+	strings.ToLower("Microsoft.Resources/conditionalaccesspolicy"): {},
+	strings.ToLower("Microsoft.Resources/directoryroles"):          {},
+	strings.ToLower("Microsoft.Resources/directorysettings"):       {},
+	strings.ToLower("Microsoft.Resources/directoryauditreport"):    {},
 }
 
 type SteampipePlugin string
