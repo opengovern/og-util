6328ca1ade732fbd433f89760487a6d3fa889d53
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-04-08 18:31:39 +0400

    feat: add vault info in describe input

--- pkg/describe/types.go
+++ pkg/describe/types.go
@@ -3,32 +3,37 @@ package describe
 import (
 	"github.com/kaytu-io/kaytu-util/pkg/describe/enums"
 	"github.com/kaytu-io/kaytu-util/pkg/source"
+	"github.com/kaytu-io/kaytu-util/pkg/vault"
 )
 
 type DescribeJob struct {
-	JobID         uint // DescribeResourceJob ID
-	ScheduleJobID uint
-	ParentJobID   uint // DescribeSourceJob ID
-	ResourceType  string
-	SourceID      string
-	AccountID     string
-	DescribedAt   int64
-	SourceType    source.Type
-	CipherText    string
-	TriggerType   enums.DescribeTriggerType
-	RetryCounter  uint
+	JobID           uint // DescribeResourceJob ID
+	ScheduleJobID   uint
+	ParentJobID     uint // DescribeSourceJob ID
+	ResourceType    string
+	SourceID        string
+	AccountID       string
+	DescribedAt     int64
+	SourceType      source.Type
+	CipherText      string
+	VaultKeyVersion string
+	TriggerType     enums.DescribeTriggerType
+	RetryCounter    uint
 }
 
 type DescribeWorkerInput struct {
-	WorkspaceId               string      `json:"workspaceId"`
-	WorkspaceName             string      `json:"workspaceName"`
-	DescribeEndpoint          string      `json:"describeEndpoint"`
-	IngestionPipelineEndpoint string      `json:"ingestionPipelineEndpoint"`
-	UseOpenSearch             bool        `json:"useOpenSearch"`
-	KeyARN                    string      `json:"keyARN"`
-	KeyRegion                 string      `json:"keyRegion"`
-	KafkaTopic                string      `json:"kafkaTopic"`
-	DescribeJob               DescribeJob `json:"describeJob"`
+	WorkspaceId               string `json:"workspaceId"`
+	WorkspaceName             string `json:"workspaceName"`
+	DescribeEndpoint          string `json:"describeEndpoint"`
+	IngestionPipelineEndpoint string `json:"ingestionPipelineEndpoint"`
+	UseOpenSearch             bool   `json:"useOpenSearch"`
+	KeyARN                    string `json:"keyARN"`
+	KeyRegion                 string `json:"keyRegion"`
+
+	VaultConfig vault.Config
+
+	KafkaTopic  string      `json:"kafkaTopic"`
+	DescribeJob DescribeJob `json:"describeJob"`
 }
 
 // Connector source.Type
