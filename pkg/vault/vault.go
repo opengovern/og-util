791a3e97d8e433ed5bb88e3ce78d8da8cb7629b1
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-04-08 14:08:23 +0400

    feat: add vault provider type

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -2,6 +2,13 @@ package vault
 
 import "context"
 
+type Provider string
+
+const (
+	AwsKMS        Provider = "aws-kms"
+	AzureKeyVault Provider = "azure-keyvault"
+)
+
 type VaultSourceConfig interface {
 	Encrypt(ctx context.Context, data map[string]any, keyId, keyVersion string) ([]byte, error)
 	Decrypt(ctx context.Context, cypherText string, keyId, keyVersion string) (map[string]any, error)
