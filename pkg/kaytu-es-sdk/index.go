946efb6b5476df4a139c0a37beb816535b9bf1c5
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-04-11 20:34:45 +0400

    feat: add list index

--- pkg/kaytu-es-sdk/index.go
+++ pkg/kaytu-es-sdk/index.go
@@ -2,6 +2,8 @@ package kaytu
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"go.uber.org/zap"
 	"io"
 )
@@ -29,4 +31,33 @@ func (c Client) CreateIndexIfNotExist(ctx context.Context, logger *zap.Logger, i
 	}
 
 	return nil
+}
+
+func (c Client) ListIndices(ctx context.Context) ([]string, error) {
+	res, err := c.es.Cat.Indices()
+	defer CloseSafe(res)
+	if err != nil {
+		return nil, err
+	} else if err := CheckError(res); err != nil {
+		if IsIndexNotFoundErr(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read response: %w", err)
+	}
+
+	var response []map[string]string
+	if err := json.Unmarshal(b, &response); err != nil {
+		return nil, fmt.Errorf("unmarshal response: %w", err)
+	}
+	indices := make([]string, 0)
+	for _, index := range response {
+		indices = append(indices, index["index"])
+	}
+
+	return indices, nil
 }
