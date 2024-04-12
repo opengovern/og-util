aa95e9a4381b74045a5dbdd27497819e36bdee06
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-04-12 18:29:56 +0400

    fix: add doc base

--- pkg/es/doc.go
+++ pkg/es/doc.go
@@ -13,6 +13,12 @@ type Doc interface {
 	KeysAndIndex() ([]string, string)
 }
 
+type DocBase map[string]any
+
+func (d DocBase) GetIdAndIndex() (string, string) {
+	return d["es_id"].(string), d["es_index"].(string)
+}
+
 func HashOf(strings ...string) string {
 	h := sha256.New()
 	for _, s := range strings {
