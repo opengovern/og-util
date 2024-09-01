cf8555ca335a051027e22d51522e9c8e70d35a74
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-09-01 17:26:09 +0400

    fix: update information cspm usage

--- pkg/api/shared-entities/information.go
+++ pkg/api/shared-entities/information.go
@@ -4,11 +4,10 @@ import "time"
 
 // CspmUsageRequest is the request object for the call home feature data gathering
 type CspmUsageRequest struct {
-	WorkspaceId               string    `json:"workspace_id"`
-	AwsOrganizationRootEmails []string  `json:"aws_organization_root_emails"`
-	AwsAccountCount           int       `json:"aws_account_count"`
-	AzureAdPrimaryDomains     []string  `json:"azure_ad_primary_domains"`
-	AzureSubscriptionCount    int       `json:"azure_subscription_count"`
-	Users                     []string  `json:"users"`
-	GatherTimestamp           time.Time `json:"gather_timestamp"`
+	WorkspaceId     string    `json:"workspace_id"`
+	GatherTimestamp time.Time `json:"gather_timestamp"`
+
+	AwsAccountCount        int `json:"aws_account_count"`
+	AzureSubscriptionCount int `json:"azure_subscription_count"`
+	ApproximateSpend       int `json:"approximate_spend"`
 }
