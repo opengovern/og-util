99ed33396e5571cb6d40ccbdbc55ea29d30b394c
artaasadi <arta-c@kaytu.io>
2024-12-18 14:03:25 +0100

    fix: add task request

---
+++ pkg/tasks/types.go
@@ -0,0 +1,19 @@
+package tasks
+
+type TaskDefinition struct {
+	RunID  uint              `json:"run_id"`
+	Params map[string]string `json:"params"`
+}
+
+type TaskRequest struct {
+	EsDeliverEndpoint         string `json:"esDeliverEndpoint"`
+	IngestionPipelineEndpoint string `json:"ingestionPipelineEndpoint"`
+	UseOpenSearch             bool   `json:"useOpenSearch"`
+
+	DescribeJob TaskDefinition `json:"describeJob"`
+
+	ExtraInputs map[string][]string `json:"extraInputs"`
+}
+
+type TaskResponse struct {
+}
